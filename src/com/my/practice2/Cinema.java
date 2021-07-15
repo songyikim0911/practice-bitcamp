dd9eec0eb682b6d2608c2bc374cd028ffec918cb
songyikim0911 <ksi6470@gmail.com>
2021-07-15 20:36:15 +0900

    tt

---
+++ src/com/my/practice2/Cinema.java
@@ -0,0 +1,20 @@
+package com.my.practice2;
+
+public class Cinema {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+    System.out.println("\t\t\t C I N E M A");
+    System.out.println("\t\t 영화 예매 프로그램");
+    System.out.println();
+    Cinema a = new Cinema();
+    try {
+      a.arrayList();
+      loop:while(true) {
+
+      }
+    }
+  }
+
+}
---
+++ src/com/my/practice2/CinemaHandler.java
@@ -0,0 +1,5 @@
+package com.my.practice2;
+
+public class CinemaHandler {
+
+}
