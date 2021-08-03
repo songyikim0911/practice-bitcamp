bb1febe080c9fa37d2f93d3724e0ac83dc5b5c7e
songyikim0911 <ksi6470@gmail.com>
2021-08-03 21:52:49 +0900

    d

---
+++ afterclass/0803/quizpractice/QuizDTO.java
@@ -0,0 +1,15 @@
+package org.zerock.quizpractice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+
+public class QuizDTO {
+
+   private int num1, num2;
+
+}
---
+++ afterclass/0803/quizpractice/QuizService.java
@@ -0,0 +1,28 @@
+package org.zerock.quizpractice;
+
+public class QuizService {
+
+    public QuizDTO makeQuiz(){
+//        int n1 = getNum();
+//        int n2 = getNum();
+//
+//        QuizDTO quizDTO = new QuizDTO(n1, n2);
+//        return quizDTO;
+//
+        return new QuizDTO(getNum(), getNum());
+    }
+
+
+    private int getNum(){
+        return (int)(Math.random()*100)+1;
+    }
+
+    public boolean grading(QuizDTO quizDTO, int answer){
+
+        int correctAnswer = quizDTO.getNum1() * quizDTO.getNum2();
+
+        return answer == correctAnswer;
+    }
+
+
+}
